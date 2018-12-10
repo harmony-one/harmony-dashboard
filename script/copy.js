a6eb42c2f54d6dd2b7f472e1371d0cc1f85d4950
Richard Liu <liuzhenglaichn@gmail.com>
2018-12-10 03:27:55 -0800

    copy

--- script/copy.js
+++ script/copy.js
@@ -10,6 +10,29 @@ copy($$('.fader.ng-scope').map(x => {
 }))
 
 // https://blockexplorer.com/block/0000000000000000001c48da2a21c9d098efcab0f4904ff4773c0896c63c62f8
+copy((function() {
+    var block = {};
+    block.height = $('h1').textContent.replace('Block #', '');
+    block.hash = $('#wrap > section > section > div:nth-child(3) > div.well.well-sm.ellipsis > span.txid.text-muted.ng-binding').textContent;
+    block.txCount = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(1) > table > tbody > tr:nth-child(1) > td.text-right.text-muted.ng-binding').textContent;
+    block.timestamp = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(1) > table > tbody > tr:nth-child(4) > td.text-right.text-muted.ng-binding').textContent;
+    block.merkleRoot = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(1) > table > tbody > tr:nth-child(6) > td.text-right.text-muted > div > span.ng-binding').textContent;
+    var prevBlock = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(1) > table > tbody > tr:nth-child(7) > td.text-right > a');
+    block.prevBlock = {
+        id: prevBlock.href.replace('https://blockexplorer.com/block/', ''),
+        height: prevBlock.textContent
+    };
+    block.bits = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(2) > table > tbody > tr:nth-child(2) > td.text-right.text-muted.ng-binding').textContent;
+    block.bytes = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(2) > table > tbody > tr:nth-child(3) > td.text-right.text-muted.ng-binding').textContent;
+    var nextBlock = $('#wrap > section > section > div:nth-child(3) > div.row > div:nth-child(2) > table > tbody > tr:nth-child(6) > td.text-right > a');
+    block.nextBlock = {
+        id: nextBlock.href.replace('https://blockexplorer.com/block/', ''),
+        height: nextBlock.textContent
+    }
+    return block;
+})())
+
+// https://blockexplorer.com/tx/a72efb891bc24cfeecf78d59a0a1672b593eaf486684e01a3883c7daa27d7684
 copy((function () {
     var tx = {};
     tx.hash = $('#wrap > section > section > div:nth-child(3) > div:nth-child(2) > div > span.txid.text-muted.ng-binding').textContent;
