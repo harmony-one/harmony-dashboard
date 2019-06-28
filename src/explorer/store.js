482ed9974ec050d725fab13e297fd5dd183bb612
Richard Liu <liuzhenglaichn@gmail.com>
2019-06-28 00:09:51 -0700

    block sort

--- src/explorer/store.js
+++ src/explorer/store.js
@@ -19,7 +19,7 @@ let store = {
         let blocks = data.blocks;
         let mergedBlocks = Object.keys(blocks)
             .reduce((memo, i) => memo.concat(blocks[i]), [])
-            .sort((a, b) => b.timestamp - a.timestamp);
+            .sort((a, b) => b.height - a.height);
         this.data.latestBlocks = mergedBlocks.concat(this.data.latestBlocks).slice(0, MaxLatestBlockCount);
 
         this.data.blockCount = data.blockCount;
