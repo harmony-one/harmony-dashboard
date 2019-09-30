5b12920f99756e7ada4339d54a6708ae168361c7
Richard Liu <liuzhenglaichn@gmail.com>
2019-09-29 23:03:37 -0700

    add addressShardPage

---
+++ src/components/AddressShardPage.vue
@@ -0,0 +1,180 @@
+<style scoped lang="less">
+@import "../less/common.less";
+</style>
+
+<template>
+  <div class="address-page explorer-page page">
+    <div class="address-body explorer-body">
+      <div class="container" v-if="!loading && address">
+        <div class="explorer-card">
+          <header>
+            <h1>Address</h1>
+          </header>
+
+          <div class="explorer-card-body">
+            <section>
+              <table class="explorer-table">
+                <tr>
+                  <td class="td-title">id</td>
+                  <td>{{ address.id }}</td>
+                </tr>
+                <tr>
+                  <td class="td-title">shard</td>
+                  <td>{{ $route.params.shardId }}</td>
+                </tr>
+                <tr>
+                  <td class="td-title">Balance</td>
+                  <td>{{ address.balance | amount }}</td>
+                </tr>
+                <tr>
+                  <td class="td-title">Transactions</td>
+                  <td>{{ address.txCount }}</td>
+                </tr>
+              </table>
+            </section>
+          </div>
+        </div>
+        <div class="explorer-card">
+          <header>
+            <h1 class="flex-grow">Transactions</h1>
+            <div class="pagination-controls">
+              <span class="total-tx-num">{{ txCount }} txs</span>
+              <span class="page-controllers">
+                <span class="page-navigator">
+                  <button
+                    class="btn btn-light btn-icon-only"
+                    @click="first()"
+                    :disabled="pageIndex === 0"
+                  >
+                    <font-awesome-icon icon="angle-double-left" />
+                  </button>
+                  <button
+                    class="btn btn-light btn-icon-only"
+                    @click="prev()"
+                    :disabled="pageIndex === 0"
+                  >
+                    <font-awesome-icon icon="chevron-left" />
+                  </button>
+                  <span class="pagination-nums">{{ pageIndex + 1 }} / {{ pageCount }}</span>
+                  <button
+                    class="btn btn-light btn-icon-only"
+                    @click="next()"
+                    :disabled="pageIndex === pageCount - 1"
+                  >
+                    <font-awesome-icon icon="chevron-right" />
+                  </button>
+                  <button
+                    class="btn btn-light btn-icon-only"
+                    @click="last()"
+                    :disabled="pageIndex === pageCount - 1"
+                  >
+                    <font-awesome-icon icon="angle-double-right" />
+                  </button>
+                </span>
+              </span>
+            </div>
+          </header>
+          <div class="explorer-card-body">
+            <table class="explorer-table">
+              <tr>
+                <th>TxHash</th>
+                <th>Timestamp</th>
+                <th>From</th>
+                <th>To</th>
+                <th>Value</th>
+              </tr>
+              <tr v-for="tx in address.txs" :key="tx.id">
+                <td>
+                  <router-link :to="'/tx/' + tx.id">{{ tx.id | shorten }}</router-link>
+                </td>
+                <td>{{ tx.timestamp | timestamp }}</td>
+                <td>
+                  <router-link :to="'/address/' + tx.from">{{ tx.from | shorten }}</router-link>
+                </td>
+                <td>
+                  <router-link :to="'/address/' + tx.to">{{ tx.to | shorten }}</router-link>
+                </td>
+                <td>{{ tx.value | amount }}</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <div class="container" v-else>
+        <loading-message />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import FontAwesomeIcon from "@fortawesome/vue-fontawesome";
+import store from "../explorer/store";
+import service from "../explorer/service";
+import LoadingMessage from "./LoadingMessage";
+
+export default {
+  name: "AddressShardPage",
+  data() {
+    return {
+      loading: true,
+      address: null,
+      txCount: 100,
+      pageIndex: 0,
+      pageCount: 25
+    };
+  },
+  components: {
+    FontAwesomeIcon,
+    LoadingMessage
+  },
+  watch: {
+    $route(to, from) {
+      this.pageIndex = (+to.params.pageIndex || 1) - 1;
+      this.getAddress();
+    }
+  },
+  mounted() {
+    this.getAddress();
+  },
+  methods: {
+    getAddress() {
+      this.loading = true;
+      service
+        .getTxHistory(
+          this.$route.params.address,
+          this.$route.params.shardId,
+          this.$route.params.pageIndex
+        )
+        .then(address => {
+          this.address = address;
+        })
+        .finally(() => {
+          this.loading = false;
+        });
+    },
+    goToPage(index) {
+      if (index < 0) index = 0;
+      if (index >= this.pageCount) index = this.pageCount - 1;
+      this.$router.replace({
+        name: "AddressShardPage",
+        params: { pageIndex: index + 1 }
+      });
+    },
+    first() {
+      this.goToPage(0);
+    },
+    last() {
+      this.goToPage(this.pageCount - 1);
+    },
+    prev() {
+      if (this.pageIndex === 0) return;
+      this.goToPage(this.pageIndex - 1);
+    },
+    next() {
+      if (this.pageIndex === this.pageCount - 1) return;
+      this.goToPage(this.pageIndex + 1);
+    }
+  }
+};
+</script>
