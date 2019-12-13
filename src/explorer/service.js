2ef56036a2bdda3b59b0e286130220109b6ca5b4
yuriy <yuriy.menkov@latoken.com>
2019-12-13 13:34:46 +0300

    Staking update

--- src/explorer/service.js
+++ src/explorer/service.js
@@ -11,7 +11,7 @@ Promise.prototype.delay = function(time) {
 };
 
 // const BACKEND_URL = `${'explorer.testnet.harmony.one'}:8888`;
-const BACKEND_URL = `${'explorer.testnet.harmony.one'}:8888`;
+const BACKEND_URL = `${window.location.hostname}:8888`;
 
 const HTTP_BACKEND_URL = `https://${BACKEND_URL}`;
 const SECRET = localStorage.getItem("secret");
