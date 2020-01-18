2fdead795504f39c02491dc4ef420ff751cdbbce
yuriy <yuriy.menkov@latoken.com>
2020-01-18 17:51:20 +0300

    Add pagination for txs-block page

--- src/components/AddressPage.vue
+++ src/components/AddressPage.vue
@@ -55,87 +55,7 @@
           </div>
         </div>
 
-        <div class="explorer-card">
-          <header>
-            <h1 class="flex-grow">Transactions</h1>
-            <div class="pagination-controls">
-              <span class="total-tx-num">{{ txCount }} txs</span>
-              <span class="page-controllers">
-                <span class="page-navigator">
-                  <button
-                    class="btn btn-light btn-icon-only"
-                    @click="first()"
-                    :disabled="pageIndex === 0"
-                  >
-                    <font-awesome-icon icon="angle-double-left" />
-                  </button>
-                  <button
-                    class="btn btn-light btn-icon-only"
-                    @click="prev()"
-                    :disabled="pageIndex === 0"
-                  >
-                    <font-awesome-icon icon="chevron-left" />
-                  </button>
-                  <span class="pagination-nums"
-                    >{{ pageIndex + 1 }} / {{ pageCount }}</span
-                  >
-                  <button
-                    class="btn btn-light btn-icon-only"
-                    @click="next()"
-                    :disabled="pageIndex === pageCount - 1"
-                  >
-                    <font-awesome-icon icon="chevron-right" />
-                  </button>
-                  <button
-                    class="btn btn-light btn-icon-only"
-                    @click="last()"
-                    :disabled="pageIndex === pageCount - 1"
-                  >
-                    <font-awesome-icon icon="angle-double-right" />
-                  </button>
-                </span>
-              </span>
-            </div>
-          </header>
-          <div class="explorer-card-body">
-            <section>
-              <table class="explorer-table">
-                <tr>
-                  <th>TxHash</th>
-                  <th>Timestamp</th>
-                  <th>From ShardID</th>
-                  <th>To ShardID</th>
-                  <th>From</th>
-                  <th>To</th>
-                  <th>Value</th>
-                  <th>Txn Fee</th>
-                </tr>
-                <tr v-for="tx in txs" :key="tx.hash">
-                  <td>
-                    <router-link :to="'/tx/' + tx.hash">{{
-                      tx.hash | shorten
-                    }}</router-link>
-                  </td>
-                  <td>{{ (Number(tx.timestamp) * 1000) | timestamp }}</td>
-                  <td>{{ tx.shardID }}</td>
-                  <td>{{ tx.toShardID }}</td>
-                  <td>
-                    <router-link :to="'/address/' + tx.from">{{
-                      tx.from | shorten
-                    }}</router-link>
-                  </td>
-                  <td>
-                    <router-link :to="'/address/' + tx.to">{{
-                      tx.to | shorten
-                    }}</router-link>
-                  </td>
-                  <td>{{ tx.value | amount }}</td>
-                  <td>{{ tx | fee }}</td>
-                </tr>
-              </table>
-            </section>
-          </div>
-        </div>
+        <transactions-table :allTxs="allTxs" />
       </div>
       <div class="container" v-else>
         <loading-message />
@@ -147,6 +67,7 @@
 <script>
 import service from '../explorer/service';
 import LoadingMessage from './LoadingMessage';
+import TransactionsTable from './TransactionsTable';
 
 export default {
   name: 'AddressPage',
@@ -154,30 +75,20 @@ export default {
     return {
       loading: true,
       address: null,
-      pageIndex: 0,
-      pageSize: 20,
       allTxs: []
     };
   },
   components: {
-    LoadingMessage
+    LoadingMessage,
+    TransactionsTable
   },
   computed: {
     txCount() {
       return this.allTxs.length;
-    },
-    pageCount() {
-      return Math.ceil(this.txCount / this.pageSize);
-    },
-    txs() {
-      const begin = this.pageIndex * this.pageSize;
-
-      return this.allTxs.slice(begin, begin + this.pageSize);
     }
   },
   watch: {
     $route() {
-      this.pageIndex = 0;
       this.getAddress();
     }
   },
@@ -205,26 +116,6 @@ export default {
           );
           this.loading = false;
         });
-    },
-    goToPage(index) {
-      if (index < 0) index = 0;
-      if (index >= this.pageCount) index = this.pageCount - 1;
-
-      this.pageIndex = index;
-    },
-    first() {
-      this.goToPage(0);
-    },
-    last() {
-      this.goToPage(this.pageCount - 1);
-    },
-    prev() {
-      if (this.pageIndex === 0) return;
-      this.goToPage(this.pageIndex - 1);
-    },
-    next() {
-      if (this.pageIndex === this.pageCount - 1) return;
-      this.goToPage(this.pageIndex + 1);
     }
   }
 };
---
+++ src/components/TransactionsTable.vue
@@ -0,0 +1,141 @@
+<style scoped lang="less">
+@import '../less/common.less';
+</style>
+
+<template>
+  <div class="transactions-table explorer-card">
+    <header>
+      <h1 class="flex-grow">Transactions</h1>
+      <div class="pagination-controls">
+        <span class="total-tx-num">{{ txCount }} txs</span>
+        <span class="page-controllers">
+          <span class="page-navigator">
+            <button
+              class="btn btn-light btn-icon-only"
+              @click="first()"
+              :disabled="pageIndex === 0"
+            >
+              <font-awesome-icon icon="angle-double-left" />
+            </button>
+            <button
+              class="btn btn-light btn-icon-only"
+              @click="prev()"
+              :disabled="pageIndex === 0"
+            >
+              <font-awesome-icon icon="chevron-left" />
+            </button>
+            <span class="pagination-nums"
+              >{{ pageIndex + 1 }} / {{ pageCount }}</span
+            >
+            <button
+              class="btn btn-light btn-icon-only"
+              @click="next()"
+              :disabled="pageIndex === pageCount - 1"
+            >
+              <font-awesome-icon icon="chevron-right" />
+            </button>
+            <button
+              class="btn btn-light btn-icon-only"
+              @click="last()"
+              :disabled="pageIndex === pageCount - 1"
+            >
+              <font-awesome-icon icon="angle-double-right" />
+            </button>
+          </span>
+        </span>
+      </div>
+    </header>
+    <div class="explorer-card-body">
+      <section>
+        <table class="explorer-table">
+          <tr>
+            <th>TxHash</th>
+            <th>Timestamp</th>
+            <th>From ShardID</th>
+            <th>To ShardID</th>
+            <th>From</th>
+            <th>To</th>
+            <th>Value</th>
+            <th>Txn Fee</th>
+          </tr>
+          <tr v-for="tx in txs" :key="tx.hash">
+            <td>
+              <router-link :to="'/tx/' + tx.hash">{{
+                tx.hash | shorten
+              }}</router-link>
+            </td>
+            <td>{{ (Number(tx.timestamp) * 1000) | timestamp }}</td>
+            <td>{{ tx.shardID }}</td>
+            <td>{{ tx.toShardID }}</td>
+            <td>
+              <router-link :to="'/address/' + tx.from">{{
+                tx.from | shorten
+              }}</router-link>
+            </td>
+            <td>
+              <router-link :to="'/address/' + tx.to">{{
+                tx.to | shorten
+              }}</router-link>
+            </td>
+            <td>{{ tx.value | amount }}</td>
+            <td>{{ tx | fee }}</td>
+          </tr>
+        </table>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TransactionsTable',
+  props: ['allTxs'],
+  data() {
+    return {
+      loading: true,
+      pageIndex: 0,
+      pageSize: 20
+    };
+  },
+  computed: {
+    txCount() {
+      return this.allTxs.length;
+    },
+    pageCount() {
+      return Math.ceil(this.txCount / this.pageSize);
+    },
+    txs() {
+      const begin = this.pageIndex * this.pageSize;
+
+      return this.allTxs.slice(begin, begin + this.pageSize);
+    }
+  },
+  watch: {
+    allTxs() {
+      this.pageIndex = 0;
+    }
+  },
+  methods: {
+    goToPage(index) {
+      if (index < 0) index = 0;
+      if (index >= this.pageCount) index = this.pageCount - 1;
+
+      this.pageIndex = index;
+    },
+    first() {
+      this.goToPage(0);
+    },
+    last() {
+      this.goToPage(this.pageCount - 1);
+    },
+    prev() {
+      if (this.pageIndex === 0) return;
+      this.goToPage(this.pageIndex - 1);
+    },
+    next() {
+      if (this.pageIndex === this.pageCount - 1) return;
+      this.goToPage(this.pageIndex + 1);
+    }
+  }
+};
+</script>
