2b2e2485061b1e83f3b9e4cb28491fe0ad268bd6
Dennis Won <jhwon0820@gmail.com>
2020-04-17 07:47:57 +0000

    nit fix. change bedir to fedir.

--- restart_fe.sh
+++ restart_fe.sh
@@ -7,11 +7,12 @@ PID=( $(pgrep npm) )
 
 FEDIR=$HOME/projects/harmony-dashboard
 
+FOUND=false
 for pid in ${PID[@]}; do
-        if sudo ls -l /proc/${pid}/cwd | grep -q 'harmony-dashboard'; then
+        if sudo ls -l /proc/${pid}/cwd | grep -qE 'harmony-dashboard$'; then
                 FOUND=true
                 kill $pid
-                pushd $BEDIR
+                pushd $FEDIR
                 npm run build
                 nohup npm run start > fe.log 2> fe.err &
                 popd
@@ -21,7 +22,7 @@ done
 
 # if no frontend process is found, just start a new one
 if [ "$FOUND" = "false" ]; then
-        pushd $BEDIR
+        pushd $FEDIR
         npm run build
         nohup npm run start > fe.log 2> fe.err &
         popd
