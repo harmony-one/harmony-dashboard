620ff3daece6756584461e1f01a2ee24022ec777
jenya <hypnagonia@gmail.com>
2020-08-27 19:42:40 +0300

    speed up tokens showing up

--- src/utils/displayAmount.js
+++ src/utils/displayAmount.js
@@ -3,7 +3,7 @@ import BigNumber from 'bignumber.js'
 export const displayAmount = (
   amount,
   decimals = 0,
-  showDecimalsFully = true
+  showDecimalsFully = false
 ) => {
   const a = new BigNumber(10).pow(decimals)
   const num = new BigNumber(amount).div(a)
